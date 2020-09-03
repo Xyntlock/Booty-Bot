a2b9f5eeae0753bd6fc1ede47b5da6357cc1fb8b
Xyntlock <70027309+Xyntlock@users.noreply.github.com>
2020-09-03 11:35:28 +0100

    Create roulette.js

---
+++ commands/roulette.js
@@ -0,0 +1,141 @@
+const { Command } = require('discord-akairo');
+const { color } = require('../config');
+
+class rouletteCommand extends Command {
+  constructor() {
+    super('roulette', {
+      aliases: ['roulette']
+    });
+  }
+
+    exec(message) {
+      let chamber = [0, 1, 2, 3, 4, 5];
+      chamber.sort(() => Math.random() - 0.5);
+      let reply = chamber[0];
+      console.log(reply);
+
+
+      const deadEmbed = {
+        color: color.yellow,
+        title: 'Russian Roulette',
+        fields: [
+          {
+            name: 'BANG. You have died.',
+            value: 'Type .roulette to play again'
+          }
+        ],
+        footer: {
+          text: 'Brought to you by Booty',
+          icon_url: 'https://pbs.twimg.com/profile_images/967080985450962944/BVy8-iVx_400x400.jpg'
+        }
+      };
+
+
+      let check = 1;
+      console.log(`check = ${check}`);
+      let left = 5;
+
+
+      const surviveEmbed = {
+        color: color.yellow,
+        title: 'Russian Roulette',
+        fields: [
+          {
+            name: 'CLICK. You live on for another turn.',
+            value: 'Type .trigger to continue.'
+          }
+        ],
+        footer: {
+          text: `Chambers left: ${left} | Brought to you by Booty`,
+          icon_url: 'https://pbs.twimg.com/profile_images/967080985450962944/BVy8-iVx_400x400.jpg'
+        }
+      };
+
+      const winEmbed = {
+        color: color.yellow,
+        title: 'Russian Roulette',
+        fields: [
+          {
+            name: 'You have survived a round of Russian Roulette! Nobody dies today.',
+            value: 'Type .roulette to play again.'
+          }
+        ],
+        footer: {
+          text: `Brought to you by Booty`,
+          icon_url: 'https://pbs.twimg.com/profile_images/967080985450962944/BVy8-iVx_400x400.jpg'
+        }
+      };
+
+      //First shot
+      if (reply === 5) {
+        message.channel.send({
+          embed: deadEmbed
+        });
+        return
+      }
+      else {
+        let reply = chamber.shift();
+        console.log(chamber);
+        message.channel.send({
+          embed: surviveEmbed
+        });
+    }
+
+    //Remaining Shots
+    function roulette() {
+      reply = chamber[0];
+      console.log(`check = ${check}`);
+
+      const survivalEmbed = {
+        color: color.yellow,
+        title: 'Russian Roulette',
+        fields: [
+          {
+            name: 'CLICK. You live on for another turn.',
+            value: 'Type .trigger to continue.'
+          }
+        ],
+        footer: {
+          text: `Chambers left: ${left} | Brought to you by Booty`,
+          icon_url: 'https://pbs.twimg.com/profile_images/967080985450962944/BVy8-iVx_400x400.jpg'
+        }
+      };
+
+      if (check === 5 && reply !== 5) {
+        message.channel.send({
+          embed: winEmbed
+        });
+        collector.stop();
+        return
+      };
+
+      if (reply === 5) {
+        message.channel.send({
+          embed: deadEmbed
+        });
+        collector.stop();
+        return
+      }
+      else {
+        reply = chamber.shift();
+        console.log(chamber);
+        message.channel.send({
+          embed: survivalEmbed
+        });
+      }
+    };
+
+    const filter = message => message.content.startsWith(['.trigger']);
+    const collector = message.channel.createMessageCollector(filter, {time: 60000 });
+
+    collector.on('collect', m => {
+      ++check;
+      --left;
+      console.log('collected');
+      roulette();
+    });
+
+  };
+};
+
+module.exports = rouletteCommand;

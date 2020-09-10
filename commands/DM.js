f0d999d63b286a7e953eac6b2ba9485743e21ebd
Xyntlock <70027309+Xyntlock@users.noreply.github.com>
2020-09-10 15:04:45 +0100

    Create DM.js

---
+++ commands/DM.js
@@ -0,0 +1,30 @@
+const { Command } = require('discord-akairo');
+
+class dmCommand extends Command {
+  constructor() {
+    super('dm', {
+      aliases: ['dm'],
+      args: [
+        {
+          id: 'username',
+          type: 'member',
+          default: message => message.member
+        },
+        {
+        id: 'words',
+        type: 'string',
+        match: 'rest'
+       }
+      ]
+    });
+  }
+
+  exec(message, args) {
+    console.log(args.username);
+    console.log(args.words);
+    args.username.user.send(`${message.member} says ${args.words}`);
+    message.channel.send('DM sent!');
+  }
+};
+
+module.exports = dmCommand;

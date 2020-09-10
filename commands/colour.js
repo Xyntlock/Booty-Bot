0da84af576617c5063038b767a299dd7060f6c56
Xyntlock <70027309+Xyntlock@users.noreply.github.com>
2020-09-10 14:13:22 +0100

    Update colour.js

--- commands/colour.js
+++ commands/colour.js
@@ -1,54 +1,123 @@
 const { Command } = require('discord-akairo');
 const Color = require('color');
+const convert = require('color-convert');
 
 class colourCommand extends Command {
   constructor() {
     super('colour', {
-      aliases: ['colour']
+      aliases: ['colour', 'color'],
+      args: [
+        {
+          id: 'numOne',
+          type: 'string',
+          default: 'random',
+          match: 'rest'
+        }
+      ]
     });
   };
 
-  exec(message) {
-    function getColor() {
-      let colorValue;
-      return colorValue = Math.floor(Math.random()*256);
-    };
-    var ranColor = Color.rgb(getColor(), getColor(), getColor());
-    var ranHex = ranColor.hex();
-    var ranRGB = ranColor.rgb().array();
-    console.log(ranHex);
+  exec(message, args) {
 
-    let string = ranHex.toString();
-    console.log(string);
-    string = string.substring(1);
-    console.log(string);
+    console.log(args.numOne);
+    args.numOne = args.numOne.toLowerCase();
 
-    const colorEmbed = {
-      color: ranHex,
-      title: 'Random Colour',
-      fields: [
+    if (args.numOne === 'random') {
+      function getColor() {
+        let colorValue;
+        return colorValue = Math.floor(Math.random()*256);
+      };
+      var ranColor = Color.rgb(getColor(), getColor(), getColor());
+      var ranHex = ranColor.hex();
+      var ranRGB = ranColor.rgb().array();
+      console.log(ranHex);
+
+      let string = ranHex.toString();
+      console.log(string);
+      string = string.substring(1);
+      console.log(string);
+
+      const colorEmbed = {
+        color: ranHex,
+        title: 'Random Colour',
+        fields: [
+          {
+          name: 'Hex Value',
+          value: ranHex,
+          inline: true
+        },
         {
-        name: 'Hex Value',
-        value: ranHex,
-        inline: true
-      },
-      {
-        name: 'RGB Values',
-        value: ranRGB,
-        inline: true
-      }
-    ],
-      image: {
-        url:`http://www.htmlcsscolor.com/preview/gallery/${string}.png`
-      },
-      footer: {
-        text: 'Brought to you by Booty',
-        icon_url: 'https://pbs.twimg.com/profile_images/967080985450962944/BVy8-iVx_400x400.jpg'
-      }
-    };
-    message.channel.send({
-      embed: colorEmbed
-    });
+          name: 'RGB Values',
+          value: ranRGB,
+          inline: true
+        }
+      ],
+        image: {
+          url:`http://www.htmlcsscolor.com/preview/gallery/${string}.png`
+        },
+        footer: {
+          text: 'Brought to you by Booty',
+          icon_url: 'https://pbs.twimg.com/profile_images/967080985450962944/BVy8-iVx_400x400.jpg'
+        }
+      };
+      message.channel.send({
+        embed: colorEmbed
+      });
+    }
+    else {
+      let words = args.numOne.split(" ");
+      let newColor;
+      try {
+        newColor = Color.rgb(words[(words.length-1)]);
+      } catch(e) {
+        message.channel.send('Please provide me with a colour.');
+      };
+
+      if (words[0] === 'light') {
+        newColor = newColor.lighten(0.2);
+      };
+      if (words[0] === 'dark') {
+        newColor = newColor.darken(0.5);
+      };
+
+      console.log(newColor);
+      let test = Color.rgb(newColor);
+      console.log(test);
+      var colorHex = test.hex();
+
+      let string = colorHex.toString();
+      console.log(string);
+      string = string.substring(1);
+      console.log(string);
+
+      const colorEmbed = {
+        color: colorHex,
+        title: args.numOne,
+        fields: [
+          {
+          name: 'Hex Value',
+          value: colorHex,
+          inline: true
+        },
+        {
+          name: 'RGB Values',
+          value: newColor.array(),
+          inline: true
+        }
+      ],
+        image: {
+          url:`http://www.htmlcsscolor.com/preview/gallery/${string}.png`
+        },
+        footer: {
+          text: 'Brought to you by Booty',
+          icon_url: 'https://pbs.twimg.com/profile_images/967080985450962944/BVy8-iVx_400x400.jpg'
+        }
+      };
+
+      message.channel.send({
+        embed: colorEmbed
+      });
+     }
   };
 };
 

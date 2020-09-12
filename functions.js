6a37b8d8d1d47e5dae32bda9d74d66554b8a2db2
Xyntlock <70027309+Xyntlock@users.noreply.github.com>
2020-09-12 23:02:54 +0100

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,4 +1,9 @@
+function capitalise(s) {
+    s = s[0].toUpperCase() + s.substring(1);
+    console.log(s);
+    return;
+};
+
 module.exports = {
-    RNG
-    alphabetically order string
-}
+  capitalise
+};

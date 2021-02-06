33d34cf7e11a1ad8dd07c9c5847b4283cf27d0b5
Xyntlock <70027309+Xyntlock@users.noreply.github.com>
2021-02-06 20:39:51 +0000

    Update selfdestruct.js
    - Can slefdestruct other users if you have Admin perm

--- commands/selfdestruct.js
+++ commands/selfdestruct.js
@@ -4,18 +4,36 @@ const { color } = require('../config');
 class destructCommand extends Command {
     constructor() {
         super('selfdestruct', {
-            aliases: ['selfdestruct']
+            aliases: ['selfdestruct'],
+            args: [
+                {
+                    id: 'name',
+                    type: 'member',
+                    default: message => message.member
+                }
+            ]
         });
     }
 
-    exec(message) {
+    exec(message, args) {
+        console.log(args.name);
+        if(args.name.id !== message.member.id) {
+            if(message.member.permissions.has('ADMINISTRATOR')) {
+                console.log(`Ok!`);
+            }
+            else {
+                message.channel.send(`You don't have the permissions for this!`);
+                return;
+            }
+        }
+        
         const destructEmbed = {
             color: color.yellow,
             title: 'Self Destruct',
             fields: [
                 {
                     name: 'Self Destruct Time',
-                    value: `${message.member} will self destruct in 5 seconds!`
+                    value: `${args.name} will self destruct in 5 seconds!`
                 }
             ],
             footer: {
@@ -39,7 +57,7 @@ class destructCommand extends Command {
         async function destructUser() {
             await sleep(5);
             let role = message.guild.roles.cache.find(r => r.name === "Self Destructed");
-             message.member.roles.add(role);
+             args.name.roles.add(role);
         }
         
         destructUser();

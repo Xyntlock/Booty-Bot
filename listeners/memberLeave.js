db8fca56d5a195b9a82864eefd68178f1d8eb926
Xyntlock <70027309+Xyntlock@users.noreply.github.com>
2022-01-21 16:14:47 +0000

    Update memberLeave.js

--- listeners/memberLeave.js
+++ listeners/memberLeave.js
@@ -9,10 +9,16 @@ class leaveListener extends Listener {
     }
 
     async exec(member) {
+        let responses = ['Seems they peeweed too close to the sun', 'Fuck', 'Bye have a great time', 'Another one bites the dust', 'Got kicked out of mosh', 'Spunky toes', 'Kassim ate them',
+    'Fucking died', 'Got arrested', 'Booteh shat on them', 'Ragequit', 'Nae nae\'d their way out the server', 'Whipped too hard', 'Crashed their car', 'Just left, innit', 'PRISM is just too naughty',
+    'PRISM got too silly', 'Bring back #economy', '*Moans*'];
+
+    let index = Math.floor(Math.random() * responses.length);
+
         console.log(`Yup`);
         
         let prism = member.guild.channels.cache.get(`447702094430863360`);
-        prism.send(`<@227848397447626752>, ${member.user.tag} (${member.displayName}) just left your server. Seems they peeweed too close to the sun.
+        prism.send(`<@227848397447626752>, ${member.user.tag} (${member.displayName}) just left your server. ${responses[index]}.
         ID of the fallen: \`${member.user.id}\``);
         console.log(`Test ${member.user}`);
     }

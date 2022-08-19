02a166910eac70b21c98b6dfaf6e77e67fee5752
JBoot1 <jboot@dunelm.com>
2022-08-19 13:54:35 +0100

    Refactor time to ts

---
+++ src/commands/time.ts
@@ -0,0 +1,60 @@
+import { Command } from 'discord-akairo'
+import type { Message } from 'discord.js'
+
+class timeCommand extends Command {
+  public constructor() {
+    super('time', {
+      aliases: ['time', 'jcbsmistall'],
+    })
+  }
+  public exec(message: Message) {
+    const d = new Date()
+
+    let year = d.getFullYear().toString()
+    let month = (d.getMonth() + 1).toString()
+    let day = d.getDate().toString()
+    let hour = d.getHours().toString()
+    let minute = d.getMinutes().toString()
+
+    function getLength(number: string) {
+      return number.length
+    }
+
+    if (getLength(month) < 2) {
+      month = `0${month}`
+    }
+    if (getLength(day) < 2) {
+      day = `0${day}`
+    }
+    if (getLength(minute) < 2) {
+      minute = `0${minute}`
+    }
+    if (getLength(hour) < 2) {
+      hour = `0${hour}`
+    }
+
+    let dateFormat = `${day}/${month}/${year}`
+    let timeFormat = `${hour}:${minute}`
+
+    const timeEmbed = {
+      color: process.env.COLOR_YELLOW,
+      title: 'Current Time',
+      fields: [
+        {
+          name: 'Time',
+          value: timeFormat,
+        },
+        {
+          name: 'DD/MM/YYYY',
+          value: dateFormat,
+        },
+      ],
+    }
+
+    message.channel.send({
+      embed: timeEmbed,
+    })
+  }
+}
+
+module.exports = timeCommand

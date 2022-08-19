6f2b45b189b5d38890428bc7ab8a18b5e38be63e
JBoot1 <jboot@dunelm.com>
2022-08-19 11:42:47 +0100

    Refactor disconnect to ts

---
+++ src/commands/disconnect.ts
@@ -0,0 +1,31 @@
+import { Command } from 'discord-akairo'
+import type { Message, GuildMember } from 'discord.js'
+
+type Arg = {
+  name: GuildMember
+}
+
+export class disconnectCommand extends Command {
+  constructor() {
+    super('disconnect', {
+      aliases: ['disconnect'],
+      args: [
+        {
+          id: 'name',
+          type: 'member',
+          default: (message: Message) => message.member,
+        },
+      ],
+    })
+  }
+
+  exec(message: Message, args: Arg) {
+    let voiceChat = args.name.voice.channel
+    console.log(voiceChat)
+    if (args.name.permissions.has('MOVE_MEMBERS')) {
+      args.name.voice.setChannel(null)
+    } else {
+      message.channel.send(`You don't have the permissions for this!`)
+    }
+  }
+}
